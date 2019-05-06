01c0f3fcab3d9c34e4fee0222b30398233dc35db
wjohnsson <wjohnsson@gmail.com>
2019-05-06 15:57:00 +0200

    Add qutebrowser config

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ Backup of my configuration files for my Linux system.
 
 First Linux-install:
 > Wed Jun 13 16:57:06 2018
-
--- backup.sh
+++ backup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# backup.sh
 # A simple script that copies my dotfiles into folders in this repo.
 
 cp ~/.bashrc .
 cp ~/.profile .
 cp ~/.Xresources .
-cp ~/.config/nvim/init.vim .
 cp ~/.config/polybar/config ./polybar/
 cp ~/.config/termite/config ./termite/
+
+# qutebrowser
+cp -r ~/.config/qutebrowser .
+
+# Editors
+cp ~/.config/nvim/init.vim .
 cp ~/.spacemacs .spacemacs
